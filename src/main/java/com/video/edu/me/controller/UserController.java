1830c1026e713d69e3ff06bf3a638d7e54ce62b5
huangguo <huangguo@xiaomi.com>
2017-12-25 14:21:52 +0800

    哇咔咔

--- src/main/java/com/video/edu/me/controller/UserController.java
+++ src/main/java/com/video/edu/me/controller/UserController.java
@@ -9,12 +9,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
 /**
  * Handles requests for the application home page.
  */
+@RequestMapping(value = "/user")
 @Controller
 public class UserController {
 
@@ -23,9 +25,11 @@ public class UserController {
 //	@Autowired
 //	UserBiz userService;
 //
-//	@RequestMapping(value = "/login", method = RequestMethod.GET)
-//	@ResponseBody
-//	private User login(String login_name, String password, HttpSession session) throws IllegalArgumentException, IllegalAccessException {
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	@ResponseBody
+	private String login(String login_name, String password, HttpSession session) throws IllegalArgumentException, IllegalAccessException {
+		return "SUCCESS";
+//		return "redirect:demo/login.html";
 //		User user = null;
 ////		user = userService.login(login_name, new Tool().encodeMD5(password));
 //		//map.put相当于request.setAttribute方法
@@ -34,7 +38,7 @@ public class UserController {
 //			session.setAttribute("user_id", user.getId());
 //		}
 //		return user;
-//	}
+	}
 
 }
 
