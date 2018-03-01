3bb2735d880b08bcc036fae3d458e2ca5df1cb9b
huangguo <huangguo@xiaomi.com>
2018-03-01 16:48:20 +0800

    ???

--- front/vue-test/src/components/HomeHeader.vue
+++ front/vue-test/src/components/HomeHeader.vue
@@ -7,52 +7,83 @@
         <el-menu-item index="2" @click="open('/home');">今日课程</el-menu-item>
         <el-menu-item index="3" @click="open('/person');">个人中心</el-menu-item>
         <el-menu-item index="4" @click="open('/updatePassword');">修改密码</el-menu-item>
+        <el-menu-item index="4" @click="logout();">退出登录</el-menu-item>
       </el-menu>
     </el-header>
     <div class="header-line"></div>
   </div>
 </template>
 <style>
-  .header-line{
+  .header-line {
     height: 5px;
     background-color: #00d1b2;
   }
-  .logo{
+
+  .logo {
     position: relative;
     margin-left: 5%;
     margin-right: 5%;
     float: left;
     z-index: 999;
   }
-  .el-menu{;
+
+  .el-menu {;
     active-text-color: #00d1b2;
   }
-  .el-menu-item{
+
+  .el-menu-item {
     font-size: 25px;
   }
-  .el-menu--horizontal>.el-menu-item.is-active, .el-menu--horizontal>.el-submenu.is-active .el-submenu__title{
+
+  .el-menu--horizontal > .el-menu-item.is-active, .el-menu--horizontal > .el-submenu.is-active .el-submenu__title {
     border-bottom: 0px;
   }
 </style>
 <script>
   export default {
     name: "homeHeader",
-    props:{
-      activeIndex:{
+    props: {
+      activeIndex: {
         type: String,
         require: false
       }
     },
-//    data() {
-//      return {
-//        activeIndex: this.activeIndex
-//      };
-//    },
+    data() {
+      return {
+        logoutUrl: '/api/user/logout'
+      };
+    },
     methods: {
-      open: function(path){
+      open: function (path) {
 //        window.open(path);
 //        this.$router.push(path);
         window.location.href = path;
+      },
+
+      logout: function (path) {
+        this.$confirm('是否确认退出?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          let token = this.getCookie("token");
+          this.$http.get(this.logoutUrl, {
+            params: {
+              token: token
+            }
+          }).then((response) => {
+            if (response.data.status == 0) {
+              this.clearCookie('token');
+              window.location.href = "/";
+              return;
+            } else if (response.data.status > 0) {
+              this.$message.error('退出失败！' + response.data.msg);
+            } else {
+              this.$message.error('退出失败！请稍后再试或联系管理员');
+            }
+          });
+        }).catch(() => {
+        });
       }
     }
   }
